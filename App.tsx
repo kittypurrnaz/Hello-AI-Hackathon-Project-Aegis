d36cf10696489fe231502c3458693dd186a6b107
Nur Nazifa Sarah Ahmad <115413009+kittypurrnaz@users.noreply.github.com>
2025-08-13 17:23:02 +0800

    Delete App.tsx

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import { ChromeExtensionToggle } from './components/ChromeExtensionToggle';
-
-export default function App() {
-  return (
-    <div className="min-h-screen bg-background flex items-center justify-center">
-      <ChromeExtensionToggle />
-    </div>
-  );
-}
