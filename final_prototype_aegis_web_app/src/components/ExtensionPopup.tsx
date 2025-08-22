2682e963595bfb0c98bd64babea77ce5f18dadec
Dissa Naik <ndissa@google.com>
2025-08-22 09:56:50 +0000

    clear folder

--- final_prototype_aegis_web_app/src/components/ExtensionPopup.tsx
+++
@@ -1,9 +0,0 @@
-import { ChromeExtensionToggle } from './ChromeExtensionToggle';
-
-export function ExtensionPopup() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-background p-4">
-      <ChromeExtensionToggle />
-    </div>
-  );
-}
